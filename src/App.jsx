248077966811bc1dd4afa3473bf120cabde4df1e
Gui204L <guiandradelima10@gmail.com>
2024-08-02 21:31:48 -0300

    p

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import {useState} from 'react'
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
+import {useState} from 'react';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Listagem from './pages/Listagem';
 
 const App = () => {
